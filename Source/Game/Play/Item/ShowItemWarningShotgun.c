e3538ce089bb56c844a6bafbf81fb0f851cea330
Brandon McGriff <nightmareci@gmail.com>
2021-01-01 09:54:31 -0800

    Fix shotgun item warning

--- Source/Game/Play/Item/ShowItemWarningShotgun.c
+++ Source/Game/Play/Item/ShowItemWarningShotgun.c
@@ -16,7 +16,7 @@ void ShowItemWarningShotgun(Player* player) {
 		entity->warningFrames = 0;
 		ENTITY_INST_DATA_PTR(BasicEntityInstanceData, data, entity);
 		data->x = 4;
-		data->x = player->screenPos[0] + player->screenOffset[0] + (data->x + 2) - (player->matrixWidth / 2) * 8;
+		data->x = player->screenPos[0] + player->screenOffset[0] + (data->x + 2) * 8 - (player->matrixWidth / 2) * 8;
 		data->y = player->screenPos[1] + player->screenOffset[1] - 96;
 		data->objectTable = OBJECTTABLE_ITEMWARNINGSHOTGUN;
 		data->palNum = 193u;
